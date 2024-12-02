e8ca87ba36eb385c1e25deec45cd87bc19f8ad48
TAGUSA Kentaro <tagusa-k@grassfield.work>
2024-12-02 18:12:05 +0900

    Initial commit

---
+++ npm-install.sh
@@ -0,0 +1,14 @@
+npm install \
+  glob \
+  rimraf \
+  userland
+
+npm install --save-dev \
+  eslint \
+  @eslint/config-array \
+  @eslint/object-schema \
+  prettier \
+  eslint-config-next \
+  eslint-config-prettier \
+  eslint-plugin-prettier \
+  prettier-plugin-tailwindcss
